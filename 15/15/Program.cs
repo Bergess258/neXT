84c334030b80f682273adb8bb90f462f5acabe4b
Berges <Berges@BERGESS>
2018-06-14 02:23:52 +0500

    ASD

--- 15/15/Program.cs
+++ 15/15/Program.cs
@@ -23,31 +23,46 @@ namespace _15
                 Adreses.Add(new Adres(Adres.RandomAdres(rand)));
             List<List<PlacesV>> Places = new List<List<PlacesV>>() { Cities, Megapolises, Adreses };
             GettingData(Places);
+            GettingCounter(Places);
             Console.WriteLine();
+        }
+
+        private static void GettingCounter(List<List<PlacesV>> Places)
+        {
+            int NumberCities = (from list in Places from place in list where place is City select place).Count();
+            int NumberCitiesL = Places.SelectMany(list => list.ToArray().Where(place => place is City).Select(place => place)).Count();
+            Func<PlacesV, bool> searchFilter = delegate (PlacesV place) { return place is City; };
+            int NumberCitiesAn = Places.SelectMany(delegate (List<PlacesV> list) { return list.ToArray().Where(searchFilter).Select(delegate (PlacesV place) { return place; }); }).Count();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Кол-во городов");
+            Console.ResetColor();
+            Console.WriteLine("linq");
+            Console.WriteLine(NumberCities);
+            Console.WriteLine("Лямбда");
+            Console.WriteLine(NumberCitiesL);
+            Console.WriteLine("Анонимные методы");
+            Console.WriteLine(NumberCitiesAn);
         }
 
         private static void GettingData(List<List<PlacesV>> Places)
         {
             var CitiesT = from list in Places from place in list where place.ToString().Length >= 9 orderby place select place;
             var CitiesL = Places.SelectMany(List => List.ToArray().Where(place => place is City).Select(place => place));
-            //Func<List<PlacesV>, bool> searchFilter = delegate (List<PlacesV> place) { return place.ToArray().Select(delegate(PlacesV pla) { return pla is City; }); };
-            //Func<PlacesV[], PlacesV[]> Item = delegate (PlacesV[] s) { return s; };
-            //Func<PlacesV, bool> searchFilter2 = delegate (PlacesV place) { return place is City; };
-            //var nameCitiesAn = Places.Where(searchFilter).OrderBy(Item).Select(Item);
-            var CitiesAn = Places.SelectMany(delegate (List<PlacesV> list) { return list.ToArray().Select(delegate (PlacesV place) { return place as City; }); }); //так и не смог нормально сделать, нулл везде если не город
+            Func<PlacesV, bool> searchFilter = delegate (PlacesV place) { return place is City; };
+            var CitiesAn = Places.SelectMany(delegate (List<PlacesV> list) { return list.ToArray().Where(searchFilter).Select(delegate (PlacesV place) { return place as City; }); }); //так и не смог нормально сделать, нулл везде если не город
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Выборка данных");
+            Console.ResetColor();
             Console.WriteLine("linq");
             foreach (PlacesV name in CitiesT)
                 Console.WriteLine(name.ToString() + " ");
             Console.WriteLine();
-
             Console.WriteLine("Лямбда");
             foreach (PlacesV name in CitiesL)
                 Console.WriteLine(name.ToString() + " ");
             Console.WriteLine();
-
             Console.WriteLine("Анонимные методы");
             foreach (PlacesV name in CitiesAn)
-                if (name != null)
                     Console.WriteLine(name.ToString() + " ");
             Console.WriteLine();
         }
